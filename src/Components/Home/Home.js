e2001b08102af1456f055d1778d19e0b9a059fb4
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-03-19 20:17:06 +0530

    Files changed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,6 +13,15 @@ const Home = () => {
                     <img src="https://i.pinimg.com/originals/78/5a/1b/785a1b9c359640da6bc9cfe3670b42ba.png" alt="" srcset="" />
                 </div>
             </div>
+            <div className="open_source">
+                <div className="open_source_image">
+
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Opensource.svg/1200px-Opensource.svg.png" alt="" srcset="" className='open_source_image' />
+                </div>
+                <div className="open_source_content">
+                    A Place where all the curated collections of OpenSouce Events/Programms and Details are organised in a serial manner
+                </div>
+            </div>
         </>
     )
 }
