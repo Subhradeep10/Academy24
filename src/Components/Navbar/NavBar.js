b751430557de4482a041239e218fa5c005a46a59
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-03-19 20:19:21 +0530

    Files changed

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -15,22 +15,18 @@ const pages = ["Open Source", "Web Dev", "Ambassador", "App Dev"];
 
 const ResponsiveAppBar = () => {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
     };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+
 
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
     };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+
 
     return (
         <AppBar position="static">
